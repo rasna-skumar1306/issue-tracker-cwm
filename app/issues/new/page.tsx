67ff057f9096245522a459966b66e267260ec71a
rasna-skumar1306 <rasna5202@gmail.com>
2024-03-16 13:42:02 +0530

    configured Inter font

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -8,8 +8,8 @@ const NewIssuePage = () => {
     <div className="max-w-xl space-y-3">
       <TextField.Root>
         <TextField.Input placeholder="Title" />
-        <TextArea placeholder="Description" />
       </TextField.Root>
+      <TextArea placeholder="Description" />
       <Button>Submit New Issue</Button>
     </div>
   );

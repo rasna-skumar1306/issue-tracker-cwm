661031a29fb3db255b95a0199bc3005b5a11e529
rasna-skumar1306 <rasna5202@gmail.com>
2024-03-23 21:06:33 +0000

    created IssueForm reusable component

---
+++ app/issues/[id]/edit/page.tsx
@@ -0,0 +1,18 @@
+import prisma from "@/prisma/client";
+import IssueForm from "../../_components/IssueForm";
+import { notFound } from "next/navigation";
+
+interface Props {
+  params: { id: string };
+}
+
+const IssueEditPage = async ({ params }: Props) => {
+  const issue = await prisma.issue.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+
+  if (!issue) notFound();
+  return <IssueForm issue={issue} />;
+};
+
+export default IssueEditPage;

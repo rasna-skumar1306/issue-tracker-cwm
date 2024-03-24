32e72d34403e0742ca2b41cf16315bbbe2cf1ea4
rasna-skumar1306 <rasna5202@gmail.com>
2024-03-24 20:47:32 +0000

    added avatar and login/logout dropdown menu

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -5,7 +5,14 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { AiOutlineBug } from "react-icons/ai";
 import { useSession } from "next-auth/react";
-import { Box, Container, Flex } from "@radix-ui/themes";
+import {
+  Avatar,
+  Box,
+  Container,
+  DropdownMenu,
+  Flex,
+  Text,
+} from "@radix-ui/themes";
 
 const Navbar = () => {
   const currentPath = usePathname();
@@ -42,7 +49,26 @@ const Navbar = () => {
           </Flex>
           <Box>
             {status === "authenticated" ? (
-              <Link href="/api/auth/signout">Log Out</Link>
+              <DropdownMenu.Root>
+                <DropdownMenu.Trigger>
+                  <Avatar
+                    src={session.user!.image!}
+                    fallback="?"
+                    size="2"
+                    radius="full"
+                    referrerPolicy="no-referrer"
+                    className="cursor-pointer"
+                  />
+                </DropdownMenu.Trigger>
+                <DropdownMenu.Content>
+                  <DropdownMenu.Label>
+                    <Text size="2">{session.user!.email}</Text>
+                  </DropdownMenu.Label>
+                  <DropdownMenu.Item>
+                    <Link href="/api/auth/signout">Log Out</Link>
+                  </DropdownMenu.Item>
+                </DropdownMenu.Content>
+              </DropdownMenu.Root>
             ) : status === "unauthenticated" ? (
               <Link href="/api/auth/signin">Log In</Link>
             ) : (

3cb0c6c6c4436c6b11037f6338a4b92c5ba30006
rasna-skumar1306 <rasna5202@gmail.com>
2024-03-24 20:50:06 +0000

    changed config for missing avatar

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [{ key: "referrer-policy", value: "no-referrer" }],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;

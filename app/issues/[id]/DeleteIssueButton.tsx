1201ce75f791fd4eb84fd1a578f83d4ec55649ed
rasna-skumar1306 <rasna5202@gmail.com>
2024-03-24 09:44:42 +0000

    handle loading when deleting issue

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Spinner } from "@/app/components";
 import { AlertDialog, Button, Flex } from "@radix-ui/themes";
 import axios from "axios";
 import { useRouter } from "next/navigation";
@@ -8,14 +9,18 @@ import { useState } from "react";
 const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
   const router = useRouter();
   const [error, setError] = useState(false);
-
+  const [isDeleting, setIsDeleting] = useState(false);
   const deleteIssue = async () => {
     try {
+      setIsDeleting(true);
       await axios.delete(`/api/issues/${issueId}`);
       router.push("/issues");
       router.refresh();
+      setIsDeleting(false);
     } catch (error) {
+      setIsDeleting(true);
       setError(true);
+      setIsDeleting(false);
     }
   };
 
@@ -23,7 +28,15 @@ const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
     <>
       <AlertDialog.Root>
         <AlertDialog.Trigger>
-          <Button color="red">Delete Issue</Button>
+          <Button color="red" disabled={isDeleting}>
+            {isDeleting && (
+              <>
+                <Spinner />
+                &nbsp;
+              </>
+            )}
+            Delete Issue
+          </Button>
         </AlertDialog.Trigger>
         <AlertDialog.Content>
           <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>

f72dfe211680649e0788ae9eb6a93847a9c4ff28
rasna-skumar1306 <rasna5202@gmail.com>
2024-03-25 13:29:51 +0000

    functional implementation of assigning and unassigning a issue

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,12 +1,12 @@
 "use client";
 
 import { Skeleton } from "@/app/components";
-import { User } from "@prisma/client";
+import { Issue, User } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
-const AssigneeSelect = () => {
+const AssigneeSelect = ({ issue }: { issue: Issue }) => {
   const {
     data: users,
     error,
@@ -23,17 +23,25 @@ const AssigneeSelect = () => {
   if (isLoading) return <Skeleton width="13rem" height="2rem" />;
 
   return (
-    <Select.Root>
+    <Select.Root
+      defaultValue={issue.assignedToUserId || " "}
+      onValueChange={(userId) => {
+        if (userId === " ") userId = "";
+        axios.patch(`/api/issues/${issue.id}`, {
+          assignedToUserId: userId || null,
+        });
+      }}
+    >
       <Select.Trigger placeholder="Assign ..." />
       <Select.Content>
         <Select.Group>
           <Select.Label>Suggestions</Select.Label>
+          <Select.Item value=" ">Unassigned</Select.Item>
           {users?.map((user) => (
             <Select.Item key={user.id} value={`${user.id}`}>
               {user.name}
             </Select.Item>
           ))}
-          {/* <Select.Item value="1">Rasswanth Senthilkumar</Select.Item> */}
         </Select.Group>
       </Select.Content>
     </Select.Root>

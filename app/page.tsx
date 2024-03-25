d6f9327add3489dcb753b39ea2627c18e50059c2
rasna-skumar1306 <rasna5202@gmail.com>
2024-03-25 23:13:21 +0000

    created issueSummary component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
+import prisma from "@/prisma/client";
+import IssueSummary from "./IssueSummary";
 import LatestIssue from "./LatestIssue";
 
-export default function Home() {
+export default async function Home() {
+  const open = await prisma.issue.count({ where: { status: "OPEN" } });
+  const closed = await prisma.issue.count({ where: { status: "CLOSED" } });
+  const inProgress = await prisma.issue.count({
+    where: { status: "IN_PROGRESS" },
+  });
+
   return (
-    <LatestIssue/>
+    <>
+      <IssueSummary closed={closed} open={open} inProgress={inProgress} />
+      <LatestIssue />
+    </>
   );
 }

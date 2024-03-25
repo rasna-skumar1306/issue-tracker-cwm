740eaa0195e327d6c50b41bc6f5aca915718e30f
rasna-skumar1306 <rasna5202@gmail.com>
2024-03-25 22:55:49 +0000

    added latest issue component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,7 @@
-import Image from "next/image";
-import Pagination from "./components/Pagination";
+import LatestIssue from "./LatestIssue";
 
 export default function Home() {
   return (
-    <main>
-      <Pagination currentPage={2} pageSize={5} itemCount={100} />
-    </main>
+    <LatestIssue/>
   );
 }
